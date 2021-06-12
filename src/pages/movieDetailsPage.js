56a734bb5b9f25247052d57a9eb7c7451e55f178
James <jamesgeraghty@live.ie>
2021-06-12 14:19:45 +0100

    Refactor: Template list page; HTTP API calls centralized

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -5,6 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
+import { getMovie, getMovieImages } from "../api/tmdb-api";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,8 +24,18 @@ const MoviePage = (props) => {
   const { id } = props.match.params;
   const [movie, setMovie] = useState(null);
   const [images, setImages] = useState([]);
+  useEffect(() => {
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
+  }, [id]);
 
   useEffect(() => {
+    getMovieImages(id).then((images) => {
+      setImages(images);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);  useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
     )
